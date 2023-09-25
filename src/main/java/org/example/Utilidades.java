861003c40e18a8a84331fa12aed682ba11cb13d5
Juank003 <jortealv@myuax.com>
2023-09-25 12:23:14 +0200

    Corregido

---
+++ src/main/java/org/example/Utilidades.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Random;
+
+public class Utilidades {
+    private static Random random = new Random();
+
+    // Genera un número aleatorio entre 0 y max (exclusivo)
+    public static int numAzar(int max) {
+        return random.nextInt(max);
+    }
+
+    // Pausa el hilo actual durante un tiempo aleatorio en milisegundos
+    public static void esperarTiempoAzar(int maxTiempo) {
+        try {
+            Thread.sleep(random.nextInt(maxTiempo));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
