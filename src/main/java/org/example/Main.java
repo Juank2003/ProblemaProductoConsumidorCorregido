f51a090f1509bc4d0c54c13e325fec5605a5259b
Juank003 <jortealv@myuax.com>
2023-09-25 12:32:58 +0200

    CorregidoMainDinamico

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        //Lanzador lanzador= new Lanzador(); no hace falta el objeto y ahorramos memoria
+        Lanzador.main(args);//llamar a la clase para las rutas dinamicas
+    }
+}
